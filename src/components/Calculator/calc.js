8ef3ef9f1f75140d350a1d4dd6b219cacc8b5831
clmitchell630 <50003943+clmitchell630@users.noreply.github.com>
2020-08-12 20:27:57 -0500

    super basic calculator mapped out

--- src/components/Calculator/calc.js
+++ src/components/Calculator/calc.js
@@ -4,20 +4,38 @@ import './calc.scss';
 
 class Calculator extends Component {
     state = {
-        number: [1,2,3,4,5,6,7,8,9,0]
+        number: [7, 8, 9, 6, 5, 4, 3, 2, 1, 0]
     }
     render() {
         return (
             <section>
-                <input type="text" placeholder="0" maxLength="15"></input>
+                <input type="text" placeholder="0" maxLength="64"></input>
                 <div>
-                    {this.state.number.map(num => (
-                        <NumButton number={num} />
-                    ))}
+                    <div className="button-row">
+                        <NumButton number="7" />
+                        <NumButton number="8" />
+                        <NumButton number="9" />
+                    </div>
+                    <div className="button-row">
+                        <NumButton number="4" />
+                        <NumButton number="5" />
+                        <NumButton number="6" />
+                    </div>
+                    <div className="button-row">
+                        <NumButton number="1" />
+                        <NumButton number="2" />
+                        <NumButton number="3" />
+                    </div>
+                    <div className="button-row">
+                        <NumButton number="0" />
+                    </div>
                 </div>
             </section>
         )
     }
 }
 
-export default Calculator;
+export default Calculator;                 
+                 
+                 
+                 
