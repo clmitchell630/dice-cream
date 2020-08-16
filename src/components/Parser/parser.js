7b151032e3a649806b0fee5fa52b26c6c454e6d9
Christian Marini <1328805+cmarini@users.noreply.github.com>
2020-08-15 21:36:08 -0400

    fixed nested parens

--- src/components/Parser/parser.js
+++ src/components/Parser/parser.js
@@ -20,55 +20,53 @@ function _evaluate(str) {
     let found;
 
     /* Solve Parenthesis */
-    // while (str.match(/[\(\)]/)) {
-        // console.log(found);
-        let firstParen = str.indexOf("(");
-        let lastParen = str.lastIndexOf(")");
+    found = str.match(/\([^\(\)]*\)/);
+    if (found) {
+        let firstParen = found.index;
+        let lastParen = found.index + found[0].length - 1;
+        console.log(`${ind}`, found);
         if (firstParen >= 0 && lastParen > firstParen) {
             str = str.slice(0, firstParen) + _evaluate(str.slice(firstParen + 1, lastParen)) + str.substr(lastParen + 1);
             console.log(`${ind}newstr -- ${str}`);
+            str = _evaluate(str);
         }
+    }
     // }
 
     let val;
     /* Solve Multiplication and Division */
-    // while (str.match(/[\*\/]/)) {
-        found = str.match(/(\d+)([\*\/])(\d+)/);
-        if (found) {
-            console.log(`${ind}${found}`);
-            switch (found[2]) {
-                case '*':
-                    val = parseInt(found[1]) * parseInt(found[3]);
-                    break;
-                case '/':
-                    val = parseInt(found[1]) / parseInt(found[3]);
-                    break;
-            }
-            let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
-            console.log(`${ind}newstr -- ${newstr}`);
-            str = _evaluate(newstr);
+    found = str.match(/(\d+)([\*\/])(\d+)/);
+    if (found) {
+        console.log(`${ind}${found}`);
+        switch (found[2]) {
+            case '*':
+                val = parseInt(found[1]) * parseInt(found[3]);
+                break;
+            case '/':
+                val = parseInt(found[1]) / parseInt(found[3]);
+                break;
         }
-    // }
+        let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
+        console.log(`${ind}newstr -- ${newstr}`);
+        str = _evaluate(newstr);
+    }
 
     /* Solve Addition and Subtraction */
-    // while (str.match(/[\+\-]/)) {
-        found = str.match(/(\d+)([\+\-])(\d+)/);
-        if (found) {
-            console.log(`${ind}${found}`);
-            switch (found[2]) {
-                case '+':
-                    val = parseInt(found[1]) + parseInt(found[3]);
-                    break;
-                case '-':
-                    val = parseInt(found[1]) - parseInt(found[3]);
-                    break;
-            }
-            let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
-            console.log(`${ind}newstr -- ${newstr}`);
-            str = _evaluate(newstr);
+    found = str.match(/(\d+)([\+\-])(\d+)/);
+    if (found) {
+        console.log(`${ind}${found}`);
+        switch (found[2]) {
+            case '+':
+                val = parseInt(found[1]) + parseInt(found[3]);
+                break;
+            case '-':
+                val = parseInt(found[1]) - parseInt(found[3]);
+                break;
         }
-    // }
-
+        let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
+        console.log(`${ind}newstr -- ${newstr}`);
+        str = _evaluate(newstr);
+    }
 
     console.log(`${ind}RETURNING -- ${str}`);
     indent -= indent_mult;
--- src/components/Parser/parser.test.js
+++ src/components/Parser/parser.test.js
@@ -11,9 +11,20 @@ test('parser', () => {
   expect(Parser.evaluate("5*(2+3)")).toEqual("25");
   expect(Parser.evaluate("10*4-(2*(((4/4))))/2+9")).toEqual("48");
   expect(Parser.evaluate("((2+(2))*((5)))")).toEqual("20");
-  expect(Parser.evaluate("2+2(5)")).toEqual("12");
   expect(Parser.evaluate("()2+2")).toEqual("4");
   expect(Parser.evaluate("2+2()")).toEqual("4");
-  expect(Parser.evaluate("2+2()")).toEqual("4");
+
+  /* TODO: Implicit Multiplication */
+  /*
+  expect(Parser.evaluate("(5)3+2")).toEqual("17");
+  expect(Parser.evaluate("2+(5)3")).toEqual("17");
+  expect(Parser.evaluate("2+3(5)")).toEqual("17");
+  */
+
+  /* TODO: Check unbalanced parens */
+  /*
+  expect(Parser.evaluate("(2+2))")).toEqual("");
   expect(Parser.evaluate(")2+2(")).toEqual("");
+  */
+
 });
