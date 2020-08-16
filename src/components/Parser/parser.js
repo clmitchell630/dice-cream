a936cdab3f5fc8a11dd6bb02af1a156a0ac4b6b1
Christian Marini <1328805+cmarini@users.noreply.github.com>
2020-08-15 21:08:10 -0400

    Basic math parsing mostly complete. Inner parens need rework

--- src/components/Parser/parser.js
+++ src/components/Parser/parser.js
@@ -1,4 +1,76 @@
 
+function eval_parens(str) {
+
+}
+
+let indent = 0;
+let indent_mult = 4;
+
 export function evaluate(str) {
+    console.log(`---- EVAL ---- ${str}`);
+    indent = 0;
+    return _evaluate(str);
+}
+
+function _evaluate(str) {
+    indent += indent_mult;
+    let ind = ' '.repeat(indent);
+    console.log(`${ind}PARSING -- ${str}`);
+
+    let found;
+
+    /* Solve Parenthesis */
+    // while (str.match(/[\(\)]/)) {
+        // console.log(found);
+        let firstParen = str.indexOf("(");
+        let lastParen = str.lastIndexOf(")");
+        if (firstParen >= 0 && lastParen > firstParen) {
+            str = str.slice(0, firstParen) + _evaluate(str.slice(firstParen + 1, lastParen)) + str.substr(lastParen + 1);
+            console.log(`${ind}newstr -- ${str}`);
+        }
+    // }
+
+    let val;
+    /* Solve Multiplication and Division */
+    // while (str.match(/[\*\/]/)) {
+        found = str.match(/(\d+)([\*\/])(\d+)/);
+        if (found) {
+            console.log(`${ind}${found}`);
+            switch (found[2]) {
+                case '*':
+                    val = parseInt(found[1]) * parseInt(found[3]);
+                    break;
+                case '/':
+                    val = parseInt(found[1]) / parseInt(found[3]);
+                    break;
+            }
+            let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
+            console.log(`${ind}newstr -- ${newstr}`);
+            str = _evaluate(newstr);
+        }
+    // }
+
+    /* Solve Addition and Subtraction */
+    // while (str.match(/[\+\-]/)) {
+        found = str.match(/(\d+)([\+\-])(\d+)/);
+        if (found) {
+            console.log(`${ind}${found}`);
+            switch (found[2]) {
+                case '+':
+                    val = parseInt(found[1]) + parseInt(found[3]);
+                    break;
+                case '-':
+                    val = parseInt(found[1]) - parseInt(found[3]);
+                    break;
+            }
+            let newstr = str.slice(0, found.index) + val + str.substr(found.index + found[0].length)
+            console.log(`${ind}newstr -- ${newstr}`);
+            str = _evaluate(newstr);
+        }
+    // }
+
+
+    console.log(`${ind}RETURNING -- ${str}`);
+    indent -= indent_mult;
     return str;
 }
--- src/components/Parser/parser.test.js
+++ src/components/Parser/parser.test.js
@@ -1,5 +1,19 @@
 import * as Parser from "./parser.js";
 
 test('parser', () => {
-  expect(Parser.evaluate("test")).toEqual("test");
+  expect(Parser.evaluate("()")).toEqual("");
+  expect(Parser.evaluate("(3)")).toEqual("3");
+  expect(Parser.evaluate("3*5")).toEqual("15");
+  expect(Parser.evaluate("4*5/2")).toEqual("10");
+  expect(Parser.evaluate("15/3")).toEqual("5");
+  expect(Parser.evaluate("1+2*5+1")).toEqual("12");
+  expect(Parser.evaluate("2+2")).toEqual("4");
+  expect(Parser.evaluate("5*(2+3)")).toEqual("25");
+  expect(Parser.evaluate("10*4-(2*(((4/4))))/2+9")).toEqual("48");
+  expect(Parser.evaluate("((2+(2))*((5)))")).toEqual("20");
+  expect(Parser.evaluate("2+2(5)")).toEqual("12");
+  expect(Parser.evaluate("()2+2")).toEqual("4");
+  expect(Parser.evaluate("2+2()")).toEqual("4");
+  expect(Parser.evaluate("2+2()")).toEqual("4");
+  expect(Parser.evaluate(")2+2(")).toEqual("");
 });
